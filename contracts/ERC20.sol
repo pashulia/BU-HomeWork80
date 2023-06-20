20ecc6b4b831dcf62f6b42e98f059330444d110b
Pavel Khegay <pashulia.pk@gmail.com>
2023-06-20 09:23:26 +0600

    fix contract

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,75 +1,116 @@
-//SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: UNLICENSED
 
 pragma solidity 0.8.19;
 
-contract ERC20 {
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function transfer(address to, uint256 amount) external returns(bool);
+    function approve(address spender, uint256 amount) external returns(bool);
+    function transferFrom(address from, address to, uint256 amount) external returns(bool);
     
-    uint256 _totalSupply;
+    function name() external view returns(string memory);
+    function symbol() external view returns(string memory);
+    function decimals() external view returns(uint8);
+    function totalSupply() external view returns(uint256);
+    function balanceOf(address account) external view returns(uint256);
+    function allowance(address owner, address spender) external view returns(uint256);
+}
+
+contract ERC20 is IERC20 {
     string _name;
     string _symbol;
+    uint256 _totalSupply;
     uint8 _decimals;
-    uint256 balance;
+    address _owner;
     
-    mapping(address => uint256) balances;  
+
+    mapping(address => uint256) balances;
     mapping(address => mapping(address => uint256)) allowed;
 
-    event Transfer(address from, address to, uint256 amount);
-    event Approval(address from, address spender, uint256 amount);
-
-    constructor (string memory name_, string memory symbol_, uint8 decimals_, uint256 amount_) {
+    constructor (string memory name_, string memory symbol_, uint8 decimals_) {
         _name = name_;
         _symbol = symbol_;
         _decimals = decimals_;
-        balances[msg.sender] = amount_;
-        _totalSupply = amount_;
+        _owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) public returns(bool) {
+        require(_owner == msg.sender, "ERC20: You are not owner!");
+        balances[to] += amount;
+        _totalSupply += amount;
+        emit Transfer(address(0), to, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) public returns(bool) {
+        require(balances[msg.sender] >= amount, "ERC20: Not enough tokens!");
+        balances[msg.sender] -= amount;
+        _totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addValue) public returns(bool) {
+        // require(allowed[msg.sender][spender] + addValue > allowed[msg.sender][spender]); для более раних версий
+        allowed[msg.sender][spender] += addValue;
+        emit Approval(msg.sender, spender, allowed[msg.sender][spender]);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subValue) public returns(bool) {
+        allowed[msg.sender][spender] -= subValue;
+        emit Approval(msg.sender, spender, allowed[msg.sender][spender]);
+        return true;
     }
     
-    function name() public view returns(string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns(string memory) {
-        return _symbol;
-    }
-
-    function decimals() public view returns(uint8) {
-        return _decimals;
-    }
-
-    function totalSupply() public view returns(uint256) {
-        return _totalSupply;
-    }
-
-    function balanceOf(address account) public returns(uint256) {
-        return balance = balances[account];
-    }
-
-    function transfer(address to, uint256 amount) public returns(bool) {
-        require(balances[msg.sender] >= amount, "ERC20: not enough tokens");
+    function transfer(address to, uint256 amount) external returns(bool) {
+        require(balances[msg.sender] >= amount, "ERC20: Not enough tokens!");
         balances[msg.sender] -= amount;
         balances[to] += amount;
         emit Transfer(msg.sender, to, amount);
         return true;
     }
 
-    function approve(address spender, uint256 amount) public returns(bool) {
+    function approve(address spender, uint256 amount) external returns(bool) {
         allowed[msg.sender][spender] = amount;
         emit Approval(msg.sender, spender, amount);
         return true;
     }
-
-    function allowance(address owner, address spender) public view returns(uint256) {
-        return allowed[owner][spender];
-    }
-
+    
     function transferFrom(address from, address to, uint256 amount) external returns(bool) {
-        require(allowed[from][msg.sender] >= amount, "ERC20: no permission to spend");
-        require(balances[from] >= amount, "ERC20: not enough tokens");
+        require(balances[from] >= amount, "ERC20: Not enough tokens!");
+        require(allowed[from][msg.sender] >= amount, "ERC20: Not enough allowed!");
         balances[from] -= amount;
         balances[to] += amount;
         allowed[from][msg.sender] -= amount;
-        emit Transfer(msg.sender, to, amount);
-        emit Approval(from, msg.sender, amount);
+        emit Transfer(from, to, amount);
+        emit Approval(from, msg.sender, allowed[from][msg.sender]);
         return true;
+    }
+
+    function name() external view returns(string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns(string memory) {
+        return _symbol;
+    }
+
+    function decimals() external view returns(uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() external view returns(uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns(uint256) {
+        return balances[account];
+    }
+
+    function allowance(address owner, address spender) external view returns(uint256) {
+        return allowed[owner][spender];
     }
 }
